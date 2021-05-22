2bc56073e090a69a0d12dcf00e93bf5172ebb6d2
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-22 14:20:50 +0530

    Add files via upload

---
+++ Namespace1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace task1
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to task1");
+            Console.WriteLine("Press Any Key to Exit..");
+            Console.ReadLine();
+        }
+    }
+}
