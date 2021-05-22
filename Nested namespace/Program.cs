4456a3bc69f7f1bcd5f3a8eab89f4196b35fd4ae
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-22 14:23:34 +0530

    Add files via upload

---
+++ Nested namespace/Program.cs
@@ -0,0 +1,32 @@
+﻿
+    using System;
+    using CustomNameSpace.Nested;
+
+    namespace program
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Welcome w = new Welcome();
+                w.GreetMessage();
+                Console.WriteLine("Press Any Key to Exit..");
+                Console.ReadLine();
+            }
+        }
+    }
+    namespace CustomNameSpace
+    {
+        namespace Nested
+        {
+            class Welcome
+            {
+                public void GreetMessage()
+                {
+                    Console.WriteLine("Welcome to task2");
+                }
+            }
+        }
+    }
+
+
